0ef89f20192454cc32262134e4a23e9985f2bef4
Lauren Riddle <lari7132@gmail.com>
2020-06-01 09:21:04 -0500

    refactored code

--- PrimeService/Processor/DeskBookingRequestProcessor.cs
+++ PrimeService/Processor/DeskBookingRequestProcessor.cs
@@ -25,9 +25,8 @@ namespace PrimeService.Processor
 
             var availableDesks = _deskRepository.GetAvailableDesks(request.Date);
 
-            if (availableDesks.Count() > 0) 
+            if (availableDesks.FirstOrDefault() is Desk availableDesk) 
             {
-            var availableDesk = availableDesks.First();
             var deskBooking = Create<DeskBooking>(request);
             deskBooking.DeskId = availableDesk.Id;
             _deskBookingRepository.Save(deskBooking);
